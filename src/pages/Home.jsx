85cf4f635698753e272468e7b4c33bfa9a392c57
erascu <e.rashku@gmail.com>
2024-08-26 20:41:06 +0300

    axios fetchData async/await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,35 +22,22 @@ function Home() {
 
     const [pizzas, setPizzas] = React.useState([]);
     const [loading, setLoading] = React.useState(true);
-    // const [categoryId, setCategoryId] = React.useState(0);
-    // const [changeSort, setChangeSort] = React.useState(0);
     const [searchValue, setSearchValue] = React.useState('');
-
-    // const [changeCat, setChangeCat] = React.useState(0);
-
-    //fetch('https://ffd7ac7335d0bda6.mokky.dev/pizzas').then(respond => respond.json()).then(items => setPizzas(items)); //this is a infinite loop, fetch should be within useEffect hook (setPizzas useState keeps rerender fetch data)
-
-    // React.useEffect(() => {
-    //     if (window.location.search) {
-    //         const params = qs.parse(window.location.search.substring(1));
-
-    //         const sortName = sortNames.find(obj => obj === params.sortId);
-    //         // const sortItem = sortItems.find(obj => obj === params.categoryId);
-
-    //         dispatch(
-    //             setFilters({ ...params, sortName }),
-    //         );
-    //     }
-    // }, []);
 
     React.useEffect(() => {
         setLoading(true);
-        const fetchData = async () => {
-            const response = await fetch(`https://ffd7ac7335d0bda6.mokky.dev/pizzas?sortBy=${sortId}${categoryId > 0 ? `&category=${categoryId}` : ''}${searchValue ? `&title=*${searchValue}*` : ''}`);
-            const data = await response.json();
-            setPizzas(data);
-            setLoading(false);
+
+        async function fetchData() {
+            try {
+                const { data } = await axios.get(`https://ffd7ac7335d0bda6.mokky.dev/pizzas?sortBy=${sortId}${categoryId > 0 ? `&category=${categoryId}` : ''}${searchValue ? `&title=*${searchValue}*` : ''}`);
+
+                setPizzas(data);
+                setLoading(false);
+            } catch (error) {
+                console.log('ERROR: ', error);
+            }
         }
+
         fetchData();
 
         window.scrollTo(0, 0);
