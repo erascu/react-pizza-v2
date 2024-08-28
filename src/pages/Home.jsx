27ff057f0bd098b5b9ffded3b05e9dfe9ca30e7f
erascu <e.rashku@gmail.com>
2024-08-28 16:20:42 +0300

    exported Business Logic from UI to Redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import qs from 'qs';
 
 import Skeleton from '../components/Skeleton';
@@ -10,35 +10,24 @@ import Search from '../components/Search/Search';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategoryId, setFilters } from '../redux/slices/filterSlice';
-import { setPizzas } from '../redux/slices/pizzasSlice';
+import { fetchPizzas } from '../redux/slices/pizzasSlice';
 import { useNavigate } from 'react-router-dom';
-// import { current } from '@reduxjs/toolkit';
 
 function Home() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const pizzas = useSelector(state => state.pizzas.items);
+    const { items, status } = useSelector(state => state.pizzas);
+    const pizzas = items;
+
     const categoryId = useSelector(state => state.filter.categoryId);
     const sortId = useSelector(state => state.filter.sortId);
 
-    // const [pizzas, setPizzas] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
     const [searchValue, setSearchValue] = React.useState('');
 
     React.useEffect(() => {
-        setLoading(true);
-
-        async function fetchData() {
-            try {
-                const { data } = await axios.get(`https://ffd7ac7335d0bda6.mokky.dev/pizzas?sortBy=${sortId}${categoryId > 0 ? `&category=${categoryId}` : ''}${searchValue ? `&title=*${searchValue}*` : ''}`);
-
-                // setPizzas(data);
-                dispatch(setPizzas(data));
-                setLoading(false);
-            } catch (error) {
-                console.log('ERROR: ', error);
-            }
+        const fetchData = async () => {
+            dispatch(fetchPizzas({ categoryId, sortId, searchValue }));
         }
 
         fetchData();
@@ -70,8 +59,15 @@ function Home() {
                     <h2 className="content__title">Pizzas</h2>
                     <Search searchValue={searchValue} setSearchValue={setSearchValue} />
                 </div>
+                {status === 'error' &&
+                    <div className="content__error">
+                        <h3>😕</h3>
+                        <h2>Oops! Something went wrong.</h2>
+                        <p>We're sorry, but we couldn't load the data. Please try again later.</p>
+                        <p>If the problem persists, please check your internet connection or contact support for further assistance.</p>
+                    </div>}
                 <div className="content__items">
-                    {loading ? [...new Array(4)].map((_, i) => <Skeleton key={i} />) : pizzas.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)} {/* {...pizza} means that all the Props that contanin Pizzas Array will be included, don't need to write "image={pizza.imageUrl} type={pizza.type} etc."*/}
+                    {status === 'loading' ? [...new Array(4)].map((_, i) => <Skeleton key={i} />) : pizzas.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
                 </div>
             </div>
         </>
--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -1,7 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchPizzas = createAsyncThunk('pizzas/fetchPizzasStatus', async (params) => {
+    const { categoryId, sortId, searchValue } = params;
+    const { data } = await axios.get(`https://ffd7ac7335d0bda6.mokky.dev/pizzas?sortBy=${sortId}${categoryId > 0 ? `&category=${categoryId}` : ''}${searchValue ? `&title=*${searchValue}*` : ''}`);
+    return data;
+});
 
 const initialState = {
     items: [],
+    status: 'loading',
 }
 
 export const pizzasSlice = createSlice({
@@ -12,6 +20,22 @@ export const pizzasSlice = createSlice({
             state.items = action.payload;
         }
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPizzas.pending, (state) => {
+                state.status = 'loading';
+                state.items = [];
+            })
+            .addCase(fetchPizzas.fulfilled, (state, action) => {
+                state.items = action.payload;
+                state.status = 'success';
+            })
+            .addCase(fetchPizzas.rejected, (state, action) => {
+                console.log(action.error.name, '-', action.error.message);
+                state.status = 'error';
+                state.items = [];
+            })
+    }
 })
 
 export const { setPizzas } = pizzasSlice.actions;
