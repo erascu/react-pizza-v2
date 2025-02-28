2cea1881557dc509db152e6072eeba63aa5f6b85
erascu <e.rashku@gmail.com>
2025-02-28 15:43:55 +0200

    add img alt description

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-    return (
-        <>
-            <div className="notfound">
-                <div className="notfound__text">
-                    <img src="/img/404.png" alt="" />
-                </div>
-                <h2><span>Oops!</span> Something went wrong!</h2>
-                <p>Page not found</p>
-                <Link to='/' className="button button--black">
-                    <span>Back to Main Page</span>
-                </Link>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="notfound">
+        <div className="notfound__text">
+          <img src="/img/404.png" alt="Cartoon pizza slice" />
+        </div>
+        <h2>
+          <span>Oops!</span> Something went wrong!
+        </h2>
+        <p>Page not found</p>
+        <Link to="/" className="button button--black">
+          <span>Back to Main Page</span>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
